69f9f5f964ae35a4f0e50bfe9132d44c9b5ebdda
Constant G <constant.guilbert@polyconseil.fr>
2023-02-28 17:12:02 +0100

    [4.6] [helper functions and unit tests] Step 4: mostBlogs function and tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,23 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((prev, curr) => (prev.likes > curr.likes ? prev : curr));
 };
 
+const mostBlogs = (blogs) => {
+  if (!blogs.length) return null;
+  const blogCountByAuthor = blogs.reduce((dict, blog) => {
+    dict[blog.author] ??= 0;
+    dict[blog.author]++;
+    return dict;
+  }, {});
+
+  const [authorWithMostBlogs, blogCount] = Object.entries(
+    blogCountByAuthor
+  ).reduce((prev, curr) => (prev[1] > curr[1] ? prev : curr));
+
+  return { author: authorWithMostBlogs, blogs: blogCount };
+};
+
 module.exports = {
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
