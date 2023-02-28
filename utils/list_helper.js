1b95097a7696df0b2b5b6edfefe5fc12db717f67
Constant G <constant.guilbert@polyconseil.fr>
2023-02-28 16:10:56 +0100

    [4.5] [helper functions and unit tests] Step 3: favorite blog function and tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,11 @@
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (!blogs.length) return null;
+  return blogs.reduce((prev, curr) => (prev.likes > curr.likes ? prev : curr));
+};
+
 module.exports = {
   totalLikes,
+  favoriteBlog,
 };
