09ee8558102798b226a4e7497d021018faeb0087
Constant G <constant.guilbert@polyconseil.fr>
2023-03-27 14:36:11 +0200

    [4.10] test that POST request creates new Blog, list length is coherent, created Blog's title is coherent

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/Blog');
 
 const api = supertest(app);
+
+const initialData = [
+  { title: 'Green', author: 'Greene', likes: 0, url: 'gr.ee.n' },
+  { title: 'Blue', author: 'Blueberry', likes: 0, url: 'blu.ee' },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany();
+
+  await Promise.all(initialData.map((blogData) => new Blog(blogData).save()));
+});
 
 test('get ze blogz', async () => {
   await api
@@ -21,6 +33,23 @@ test('blogs have an id', async () => {
   expect(body[0].id).toBeDefined();
 });
 
+test('add one', async () => {
+  await api
+    .post('/api/blogs')
+    .send({
+      title: 'Benjie',
+      author: 'Turtle McFly',
+      likes: 0,
+      url: 'Blueberry',
+    })
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const { body } = await api.get('/api/blogs');
+
+  expect(body.length).toEqual(3);
+  expect(body[2]).toHaveProperty('title', 'Benjie');
+});
 afterAll(async () => {
   await mongoose.connection.close();
 });
